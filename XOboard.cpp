fc303e7c30346a52a68cc85ae619adda34b8662b
kinan-not-found <kinanotfound2021@gmail.com>
2022-05-27 12:25:52 +0300

    Adding rounds counter and a player finder

--- XOboard.cpp
+++ XOboard.cpp
@@ -9,15 +9,18 @@ XOboard::XOboard()
 	std::cin >> sign_1;
 	std::cout << "Player 2 : Enter the sign you want to use : ";
 	std::cin >> sign_2;
-	for (size_t i = 0; i < 100; i++)
+	system("cls");
+	do
 	{
 		moving();
-	}
+	} while (round != 10);
+	boardShow();
 	std::cout << "end.." << std::endl;
 }
 void XOboard::boardInit()
 {
 	turn = 0;
+	round = 1;
 	for (size_t i = 0; i < 3; i++)
 		for (size_t j = 0; j < 3; j++)
 			boardArray[i][j] = emptyChar;
@@ -26,29 +29,34 @@ void XOboard::boardInit()
 }
 void XOboard::boardShow()
 {
+	if (round == 1)
+	{
+		std::cout << "  <-Keyboard buttons->" << std::endl << std::endl;
+		std::cout << "  Moving : w, s, a, d" << std::endl << std::endl;
+		std::cout << "  Placing : -space button-" << std::endl << std::endl;
+		std::cout << "  Restarting : r" << std::endl << std::endl;
+	}
+	std::cout << "               round : " << round;
+	if (turn % 2 == 0)
+		std::cout << " ( player-1 )" << std::endl << std::endl;
+	else
+		std::cout << " ( player-2 )" << std::endl << std::endl;
+	std::cout << "                    ( " << position_x << " , " << position_y << " )" << std::endl << std::endl;
 	for (size_t i = 0; i < 3; i++)
 	{
 		for (size_t j = 0; j < 3; j++)
 		{
-			std::cout << boardArray[i][j] << "		";
+			std::cout << boardArray[i][j] << "			";
 		}
-		std::cout << std::endl << std::endl;
+		std::cout << std::endl << std::endl << std::endl << std::endl;
 	}
-}
-void XOboard::nextPosition(unsigned int pX, unsigned int pY)
-{
-	if (0 <= pX && pX < 3 && 0 <= pY && pY < 3)
-	{
-		position_y = pX;
-		position_x = pY;
-	}
-	else
-		std::cout << std::endl << "Unvalid position!" << std::endl;
 }
 void XOboard::set(char XO)
 {
 	if (boardArray[position_y][position_x] == '*')
 	{
+		turn++;
+		round++;
 		boardArray[position_y][position_x] = XO;
 	}
 	else
@@ -100,10 +108,33 @@ void XOboard::moving()
 		{
 			set(sign_2);
 		}
-		turn++;
 		break;
+	case 'r':
+		boardInit();
 	default:
 		break;
 	}
 	system("cls");
+}
+bool XOboard::winChecker()
+{
+	//if ((boardArray[0][0] != emptyChar) == (boardArray[1][1] != emptyChar) == (boardArray[2][2] != emptyChar))
+	//	if (boardArray[0][0] == boardArray[1][1] == boardArray[2][2])
+	//		return true;
+	//if (boardArray[2][2] == boardArray[2][1] == boardArray[2][0] != emptyChar)
+	//	return true;
+	//if (boardArray[0][0] == boardArray[0][1] == boardArray[0][2] != emptyChar)
+	//	return true;
+	//if (boardArray[1][0] == boardArray[1][1] == boardArray[1][2] != emptyChar)
+	//	return true;
+	//if (boardArray[2][0] == boardArray[2][1] == boardArray[2][2] != emptyChar)
+	//	return true;
+	//if (boardArray[0][0] == boardArray[1][0] == boardArray[2][0] != emptyChar)
+	//	return true;
+	//if (boardArray[0][1] == boardArray[1][1] == boardArray[2][1] != emptyChar)
+	//	return true;
+	//if (boardArray[0][2] == boardArray[1][2] == boardArray[2][2] != emptyChar)
+	//	return true;
+	//return false;
+	return true;
 }
--- XOboard.h
+++ XOboard.h
@@ -2,14 +2,14 @@
 class XOboard
 {
 private:
-	int turn;
+	int round, turn;
 	unsigned int position_y, position_x;
 	char boardArray[3][3], sign_1, sign_2, emptyChar, inputKey;
 public:
 	XOboard();
 	void boardInit();
 	void boardShow();
-	void nextPosition(unsigned int, unsigned int);
 	void moving();
 	void set(char);
+	bool winChecker();
 };
